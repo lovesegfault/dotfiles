9fa9860d2559ddda1f0203ca6a1d4cc96d11aecc
Bernardo Meurer <meurerbernardo@gmail.com>
2018-05-22 23:29:29 -0700

    Forced copies

--- update.bash
+++ update.bash
@@ -4,18 +4,18 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-cp ~/.config/alacritty/alacritty.yml ./alacritty.yml
-cp /etc/fstab ./
-cp /etc/default/grub ./grub
+cp -f ~/.config/alacritty/alacritty.yml ./alacritty.yml
+cp -f /etc/fstab ./
+cp -f /etc/default/grub ./grub
 
-cp ~/.config/i3status/config ./i3status.config
-cp ~/.config/i3/config ./i3.config
-cp ~/.config/i3/i3status-rs.toml ./i3status-rs.toml
+cp -f ~/.config/i3status/config ./i3status.config
+cp -f ~/.config/i3/config ./i3.config
+cp -f ~/.config/i3/i3status-rs.toml ./i3status-rs.toml
 
-cp ~/.config/nvim/init.vim ./init.vim
-cp ~/.config/pacman/makepkg.conf ./makepkg.conf
-cp ~/.tmux.conf ./tmux.conf
-cp /etc/vconsole.conf ./vconsole.conf
-cp ~/.xinitrc ./xinitrc
-cp ~/.zprofile ./zprofile
-cp ~/.zshrc ./zshrc
+cp -f ~/.config/nvim/init.vim ./init.vim
+cp -f ~/.config/pacman/makepkg.conf ./makepkg.conf
+cp -f ~/.tmux.conf ./tmux.conf
+cp -f /etc/vconsole.conf ./vconsole.conf
+cp -f ~/.xinitrc ./xinitrc
+cp -f ~/.zprofile ./zprofile
+cp -f ~/.zshrc ./zshrc
