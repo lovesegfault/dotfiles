fc0491519041b756c1e46cd33ece5243dc49b494
Bernardo Meurer <meurerbernardo@gmail.com>
2018-12-26 14:26:14 -0200

    Updated update script

--- update.bash
+++ update.bash
@@ -7,14 +7,19 @@ cp -f ~/.config/alacritty/alacritty.yml ./alacritty.yml
 cp -f /etc/fstab ./
 cp -f /etc/default/grub ./grub
 
-cp -f ~/.config/i3status/config ./i3status.config
 cp -f ~/.config/i3/config ./i3.config
-cp -f ~/.config/i3/i3status-rs.toml ./i3status-rs.toml
+cp -f ~/.config/i3status-rs.toml ./i3status-rs.toml
 
 cp -f ~/.config/nvim/init.vim ./init.vim
-cp -f ~/.config/pacman/makepkg.conf ./makepkg.conf
 cp -f ~/.tmux.conf ./tmux.conf
 cp -f /etc/vconsole.conf ./vconsole.conf
 cp -f ~/.xinitrc ./xinitrc
-cp -f ~/.zprofile ./zprofile
+cp -f ~/.Xresources ./Xresources
 cp -f ~/.zshrc ./zshrc
+cp -r ~/bin ./
+
+cp -f /etc/genkernel.conf ./genkernel.conf
+cp -f /etc/portage/make.conf ./make.conf
+cp -f /usr/src/linux/.config ./kernel.config
+cp -r ~/pictures/walls ./
+
