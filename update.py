0dd3fe7109654c0b3324d59cd4462560e624d9d9
Bernardo Meurer <meurerbernardo@gmail.com>
2019-07-10 22:51:08 -0700

    Implemented basic copy

--- update.py
+++ update.py
@@ -3,8 +3,13 @@
 import os
 import platform
 from logzero import logger
+from pathlib import Path
 
-class UpdateError:
+home_dir = Path(os.environ['HOME'])
+system = platform.system()
+
+
+class UpdateError(BaseException):
     """ Raised for errors during dotfile update process.
 
     Attributes:
@@ -16,20 +21,76 @@ class UpdateError:
         self.stage = stage
         self.message = message
 
+
 def get_picture_dir():
-    system = platform.system()
     if system == "Linux":
-        return "{}/pictures".format(os.environ['HOME'])
+        return home_dir / "pictures"
     elif system == "Darwin":
-        return "{}/Pictures".format(os.environ['HOME'])
+        return home_dir / "Pictures"
     else:
-        None
+        raise UpdateError("root-mapping", "Unknown OS: {}".format(system))
+
+
+def get_config_dir():
+    if system == "Linux":
+        return home_dir / ".config"
+    elif system == "Darwin":
+        return home_dir / ".config"
+    else:
+        raise UpdateError("config-mapping", "Unknown OS: {}".format(system))
+
+
+config_mapping = {
+    "alacritty.yml": get_config_dir() / "alacritty",
+    "dracut.conf": Path("/etc/dracut.conf"),
+    "fstab": Path("/etc/fstab"),
+    "genkernel.conf": Path("/etc/genkernel.conf"),
+    "grub": Path("/etc/default/grub"),
+    "i3": get_config_dir() / "i3",
+    "i3status-rs.toml": get_config_dir() / "i3status-rs.toml",
+    "make.conf": Path("/etc/portage/make.conf"),
+    "nvim": get_config_dir() / "nvim",
+    "rofi": get_config_dir() / "rofi",
+    "sway": get_config_dir() / "sway",
+    "tmux.conf": home_dir / ".tmux.conf",
+    "vconsole.conf": Path("/etc/vconsole.conf"),
+    "xinitrc": home_dir / ".xinitrc",
+    "Xresources": home_dir / ".Xresources",
+    "zshrc": home_dir / ".zshrc",
+}
 
 root_mapping = {
-        "bin": "{}/bin".format(os.environ['HOME']),
-        "walls": "{}/walls".format()
-        }
+    "bin": home_dir / "bin",
+    "walls": get_picture_dir() / "walls",
+    "config": config_mapping,
+    "kernel": None
+}
 
-logger.info('Starting dotfile update')
+def rsync_copy(src, dst):
+    cmd = "rsync -Pav {} {}".format(src, dst)
+    logger.debug(cmd)
+    os.system(cmd)
+
+def handle_copy(src, dst):
+    rsync_copy(src,dst)
+
+
+def handle_mapping(mapping):
+    pass
+
+
+logger.info("Starting dotfile update")
+for key in root_mapping:
+    value = root_mapping[key]
+    if isinstance(value, dict):
+        logger.info("Handling {} as mapping".format(key))
+        handle_mapping(value)
+    elif isinstance(value, Path):
+        logger.info("Handling {} as copy".format(key))
+        handle_copy(value, key)
+    else:
+        logger.info("Skipping {}".format(key))
+
+
 
 
